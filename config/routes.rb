5448aae5443394068599a2412981e9f4d4cb3dfa
Keagan Barnes <barnes.keagan9@gmail.com>
2020-01-03 16:58:45 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :applications do
+      resources :interviews
+    end
+    resources :interviews do
+      resources :todos
+    end
+  end
 end
