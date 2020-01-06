97b1cddd465ee5d0d8d872b8621f3f468152a3c9
Abigail Mitchell <abby.w.mitchell@gmail.com>
2020-01-06 16:24:41 -0700

    job controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :applications do
+
+    resources :jobs do
       resources :interviews
     end
 
-    resources :interviews do
-      resources :todos
+    resources :jobs do
+      resources :tasks
     end
+
+    resources :jobs do
+      resources :contacts
+    end
+
+    resources :jobs do
+      resources :notes
+    end
+
   end
+  
 end
--- db/migrate/20200103222710_add_company_and_job_title_to_applications.rb
+++
@@ -1,6 +0,0 @@
-class AddCompanyAndJobTitleToApplications < ActiveRecord::Migration[6.0]
-  def change
-    add_column :applications, :company_name, :string
-    add_column :applications, :job_title, :string
-  end
-end
