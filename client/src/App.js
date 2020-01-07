a341f873001da220ffec7681e0da285a7d1675e4
Abigail Mitchell <abby.w.mitchell@gmail.com>
2020-01-06 19:05:11 -0700

    notes form added using hooks/the custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Jobs from './components/Jobs'
 import NoMatch from './components/NoMatch';
 import Navbar from './components/Navbar';
+import NotesForm from './components/NotesForm';
 import Login from './components/Login';
 import Register from './components/Register';
 import FetchUser from './components/FetchUser';
@@ -17,6 +18,7 @@ const App = () => (
         <Switch>
           <ProtectedRoute exact path="/" component={Home} />
           <ProtectedRoute exact path='/jobs' component={Jobs} />
+          <ProtectedRoute exact path="/jobs/:job_id/new_note" component={NotesForm} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route component={NoMatch} />
---
+++ client/src/components/NotesForm.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import axios from 'axios'
+
+import useFormInput from '../hooks/useFormInput'
+
+const NotesForm = (props) => {
+
+  const body = useFormInput('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/:job_id/new_note', { body: body.value, })
+    .then(res => {
+        props.add(res.data);
+      })
+  };
+
+  // Form component
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        Note <br/>
+        <input 
+          type="text" 
+          name="body" 
+          {...body} />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
+export default NotesForm
+
+
