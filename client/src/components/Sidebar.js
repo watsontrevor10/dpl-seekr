0e2986675668c3eaa62511ffda26fc9d621df1d7
Abigail Mitchell <abby.w.mitchell@gmail.com>
2020-01-09 16:22:34 -0700

    added logout button to sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom"; 
+import { AuthConsumer, } from "../providers/AuthProvider";
+import { Link, withRouter, } from 'react-router-dom'
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  const { auth: { user, handleLogout, }, location, } = props;
   return(
     <>
-    <div className="sidebar">
-      <h1>DPL Logo</h1>
-      <div>home</div>
-      <div>username</div>
-    </div>
+      <div className="sidebar">
+        <h1>DPL Logo</h1>
+        <div>home</div>
+        <div>username</div>
+      </div>
+      <button
+          className="logout-btn"
+          name='logout'
+          onClick={ () => handleLogout(props.history) }
+        >logout
+      </button>
     </>
   );
 }
 
-export default Sidebar; 
+export class ConnectedSidebar extends React.Component {
+  render() {
+    return (
+      <AuthConsumer> 
+        { auth => 
+          <Sidebar { ...this.props } auth={auth} />
+        }
+      </AuthConsumer>
+    )
+  }
+}
+
+export default withRouter(ConnectedSidebar); 
