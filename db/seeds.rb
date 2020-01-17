3fe9965f671f65b78fd5ef3144c4b7a282f778e0
Trevor Watson <watsontrevor10@gmail.com>
2020-01-16 17:19:55 -0700

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,91 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+status = [
+  'Wishlist',
+  'Applied', 
+  'Offer',
+  'Interviewed', 
+  'Rejected',
+]
+
+interview_type = [
+  'Phone',
+  'In-Person',
+  'Code Challenge',
+  'Whiteboard',
+]
+
+department = [
+  'Software Engineering',
+  'Recruiting',
+  'Quality Assurance',
+  'Software Testing',
+  'Dev Ops',
+]
+
+task_verbs = [
+  'Call',
+  'Follow-up with', 
+  'Text', 
+  'Email',
+]
+
+
+1.times do
+  User.create(
+    email: 'test@test.com',
+    password: 'password',
+    name: 'Kenny G'
+  )
+end
+
+20.times do
+  j = Job.create(
+    job_title: Faker::Job.title,
+    salary: Faker::Number.between(from: 30000, to: 120000),
+    status: status.sample,
+    company_name: Faker::Company.name,
+    date_applied: Faker::Date.between(from: 30.days.ago, to: Date.today),
+    location: Faker::Address.city,
+    user_id: 1, 
+  )
+  
+  2.times do
+    i = j.interviews.create(
+      date: Faker::Date.between(from: 10.days.ago, to: 15.days.from_now),
+      subject: 'Interview',
+      description: Faker::Marketing.buzzwords,
+      interview_type: interview_type.sample,
+    )
+  end
+
+  10.times do
+    n = j.notes.create(
+      body: Faker::Lorem.paragraphs(number: 1),
+    )
+  end
+
+  5.times do
+    t = j.tasks.create(
+      due_date: Faker::Date.forward(days: 30),
+      subject: task_verbs.sample + ' ' + Faker::Name.first_name + ' ' + 'about' + ' ' + Faker::Hacker.adjective, 
+    )
+  end
+
+  3.times do
+    c = j.contacts.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      phone: Faker::PhoneNumber.cell_phone,
+      email: Faker::Internet.email, 
+      position: Faker::Job.position,
+      department: department.sample,
+      description: Faker::Company.catch_phrase,
+    )
+  end
+
+end
+
+puts 'Data Seeded'
+
+
+
+
