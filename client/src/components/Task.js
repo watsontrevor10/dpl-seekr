7cec69fb95d805793f40d49f4cfcfd9290f0014f
Jacob Bradford <jcbbrdfrd@yahoo.com>
2020-01-17 16:07:31 -0700

    fixed double click checkbox in tasks

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -2,25 +2,29 @@ import React, { useState, useEffect, } from 'react';
 import axios from "axios";
 
 const Task = (props) => {
-  const [completed, setCompleted] = useState()
-  const {due_date, subject, completed_date,} = props.task
+  const {due_date, subject, completed_date, completed} = props.task
 
   useEffect( () => {
-    setCompleted(props.task.completed)
+    console.log(completed)
   },[])
   
+  const handleChange = (e) => {
+    e.preventDefault();
+    handleCompleted();
+  }
+  
   const handleCompleted = () => {
-    setCompleted(!completed);
-    const newTask = { due_date, subject, completed_date, completed: completed }
+    const newTask = { due_date, subject, completed_date, completed: !completed }
     axios.put(`/api/jobs/${props.task.job_id}/tasks/${props.task.id}`, newTask)
     .then(res => {
       props.handleUpdate();
+      
     })
   }
 
   return (
     <>
-      <input type="checkbox" name="completed" onChange={handleCompleted} checked={props.task.completed}/>
+      <input type="checkbox" name="completed" onChange={handleChange} checked={completed}/>
       <p>{props.task.completed ? "True" : "False"}</p>
       <p>{props.task.subject}</p>
       <p>{props.task.due_date}</p>
