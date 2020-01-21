6537baca5e409c972ae1a95392db374d36ebdfda
Jacob Bradford <jcbbrdfrd@yahoo.com>
2020-01-21 14:09:39 -0700

    added styling to delete confirmation modal

--- client/src/components/DeleteModal.js
+++ client/src/components/DeleteModal.js
@@ -8,9 +8,9 @@ const DeleteModal = (props) => {
     {/* <Backdrop show={props.show} hide={props.hide} /> */}
     <div className="delete-container">
       <div className="delete-header">
-        <h3>Are you sure you would like to delete this Job?</h3>
-        <button onClick={() => props.delete(props.id) }>Yes</button>
-        <button onClick={() => props.hide()}>No</button>
+        <h3 className="delete-confirmation-text">Are you sure you would like to delete this?</h3>
+        <button className="modal-yes" onClick={() => props.hide()}>No</button>
+        <button className="modal-info-delete-btn" onClick={() => props.delete(props.id) }>Delete</button>
       </div>
     </div>
     </>
