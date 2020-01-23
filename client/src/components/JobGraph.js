c30b5a8612278174482568cc43123a2bcbe7ef46
Trevor Watson <watsontrevor10@gmail.com>
2020-01-23 11:32:51 -0700

    added linear gradient to charts

--- client/src/components/JobGraph.js
+++ client/src/components/JobGraph.js
@@ -42,10 +42,16 @@ class JobGraph extends React.Component {
             top: 10, right: 30, left: 0, bottom: 0,
           }}
         >
+          <defs>
+            <linearGradient id="color1" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#3d1a68" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#3d1a68" stopOpacity={0.2} />
+            </linearGradient>
+          </defs>
           <XAxis dataKey="0" />
           <YAxis />
           <Tooltip />
-          <Bar dataKey="1" fill="#3d1a68" />
+          <Bar dataKey="1" fill="url(#color1)" />
         </BarChart>
       </>
     )
