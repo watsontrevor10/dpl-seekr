c123dff24adba5c7035c7b3dcea22d76bfa7d2f7
Aleem Pathammavong <Aleemp321@yahoo.com>
2020-01-23 10:20:20 -0700

    merge conflict

--- client/src/components/JobViewForm.js
+++ client/src/components/JobViewForm.js
@@ -46,12 +46,13 @@ const JobViewForm = (props) => {
   return (
     <>
     {success ? <div className="message"><h3 className="success">Successful!</h3></div> : null }
+    <div className="container">
     <div className="notes-container">
       <h2 className="form-heading">{props.job.job_title}</h2>
       <button className="job-card-delete" onClick={() => props.showDelete() }>
         Delete this job
       </button>
-    </div>
+      </div>
       <form onSubmit={handleSubmit} className="jobview-form info">
         <div className="all-inputs info-input">
           <div className="form-input">
@@ -113,13 +114,14 @@ const JobViewForm = (props) => {
           <button 
             className="jobinfo-save-btn info"
             type="submit" value="Submit"
-          > 
+            > 
             save
           </button>
             <button className="job-card-delete-mobile" onClick={() => props.showDelete() }>
               Delete this job
             </button>
       </form>
+      </div>
       {props.deleteModal ? <DeleteConfirmationModal show={props.showDelete} hide={props.hideDelete} delete={props.delete} id={props.job.id} /> : null }
     </>
   )
