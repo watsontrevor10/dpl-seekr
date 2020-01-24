ded87dd0587396c7bba0f7ad85ce29864ce03b0e
Jacob Bradford <jcbbrdfrd@yahoo.com>
2020-01-24 14:27:41 -0700

    fixed delete issue in contacts

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import ContactForm from './ContactForm'
 import axios from 'axios'
 import DeleteModal from "./DeleteModal";
+import Contact from './Contact';
 
 const Contacts = (props) => {
   // State for looping contacts on page
@@ -10,17 +11,6 @@ const Contacts = (props) => {
   const [editContact, setEditContact] = useState(null)
   // State for toggling ContactForm add/edit form
   const [toggleForm, setToggleForm] = useState(false)
-  // State for toggling delete confirmation modal 
-  const [deleteModal, setDeleteModal] = useState(false)
-
-
-  const toggleDelete = () => {
-    setDeleteModal(true)
-  }
-  const hideDelete = () => {
-    setDeleteModal(!deleteModal)
-  }
-
 
   useEffect(() => {
     axios.get(`/api/jobs/${props.id}/contacts`)
@@ -34,7 +24,6 @@ const Contacts = (props) => {
     axios.delete(`/api/jobs/${props.id}/contacts/${id}`)
       .then(res => {
         setContacts(contacts.filter(c => c.id !== id))
-        hideDelete()
       })
   }
 
@@ -67,41 +56,11 @@ const Contacts = (props) => {
   // render all contacts
   const renderContacts = (props) => {
     return contacts.map((contact, index) => (
-      <>
-        <div key={contact.id} className="contact-card">
-          <div className="contact-content">
-            <h4>Name</h4>
-            <p>{contact.first_name} {contact.last_name}</p>
-          </div>
-          <div className="contact-content">
-            <h4>Phone </h4>
-            <p>{contact.phone}</p>
-          </div>
-          <div className="contact-content">
-            <h4>Email </h4>
-            <p>{contact.email}</p>
-          </div>
-          <div className="contact-content">
-            <h4>Position</h4> 
-            <p>{contact.position}</p>
-          </div>
-          <div className="contact-content">
-            <h4>Department</h4> 
-            <p>{contact.department}</p>
-          </div>
-          <div className="contact-content desc">
-            <h4>Description</h4>
-            <p>{contact.description}</p>
-          </div>
-            <div className="contact-btns">
-              <button onClick={() => handleEdit(index)} className="edit-btn">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-              </button>
-              <button onClick={() => toggleDelete()} className="modal-info-delete-btn">Delete</button>
-            </div>
-            { deleteModal ? <DeleteModal show={toggleDelete} hide={hideDelete} delete={handleRemove} id={contact.id} />: null }
-        </div>
-      </>
+      <Contact 
+        contact={contact} 
+        handleEdit={handleEdit} 
+        index={index}   
+        handleRemove={handleRemove} />
     ))
   }
 
